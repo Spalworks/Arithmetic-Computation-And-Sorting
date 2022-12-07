a22c56fe2236c7240283c2e35ce8eb7356a2ed82
spalworks <spalworks@gmail.com>
2022-12-07 23:43:10 +0530

    [Sourav]-UC4 compute committed successfully

---
+++ UC4compute.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -p "Enter Number A:" a
+read -p "Enter Number B:" b
+read -p "Enter Number C:" c
+
+answer1=$(($a + $b * $c))
+echo "$answer1"
+
+answer2=$(($a * $b + $c))
+echo "$answer2"
+
+answer3=$(($c + $a / $b))
+echo "$answer3"
