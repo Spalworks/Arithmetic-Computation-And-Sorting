c71192e109112f77d830f2cf5afbd4874a20f27f
spalworks <spalworks@gmail.com>
2022-12-07 23:45:50 +0530

    [Sourav]-UC5 compute committed successfully

---
+++ UC5compute.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+read -p "Enter Number A:" a
+read -p "Enter Number B:" b
+read -p "Enter Number C:" c
+
+answer1=$(($a + $b * $c))
+echo "$answer1"
+
+answer2=$(($a * $b + $c))
+echo "$answer2"
+
+answer3=$(($c + $a / $b))
+echo "$answer3"
+
+answer4=$(($a % $b + $c))
+echo "$answer4"
